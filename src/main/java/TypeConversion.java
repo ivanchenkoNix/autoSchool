6a67db4f18e7ead4471e907bdc58e1bf649fba51
ivanchenkoNix <ivanchenko.d@nixsolutions.com>
2019-07-16 18:23:03 +0300

    added type conversion tasks4

--- src/main/java/TypeConversion.java
+++ src/main/java/TypeConversion.java
@@ -1,25 +1,26 @@
 public class TypeConversion {
 
     public static void main(String args[]) {
-       task1();
-       task2();
-       task3();
+        task1();
+        task2();
+        task3();
+        task4();
     }
 
     private static void task1() {
         int q = 21;
         int c = 8;
-        System.out.println(q/c%10+ " ostatok "+ q%c);
+        System.out.println(q / c % 10 + " ostatok " + q % c);
     }
 
     private static void task2() {
         int n = 49;
-        System.out.println(n+=n);
+        System.out.println(n += n);
     }
 
     private static void task3() {
-       double n = 32.13;
-       System.out.println(Math.round(n));
+        double n = 32.13;
+        System.out.println(Math.round(n));
     }
 
     private static void task4() {
@@ -27,5 +28,15 @@ public class TypeConversion {
         int b = 2;
         int c = 5;
 
+        int a2 = a + b;
+        int b2 = c - a;
+        int c2 = a + b + c;
+
+        a = a2;
+        b = b2;
+        c = c2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
