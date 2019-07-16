ded64fc417af2f916c2d903caeb06ccefb4914b7
ivanchenkoNix <ivanchenko.d@nixsolutions.com>
2019-07-16 15:49:25 +0300

    added first task

---
+++ src/main/java/SimpleDataTypesTask.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SimpleDataTypesTask {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        printHelloName();
+
+    }
+
+    static void printHelloName() {
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.println("Hello, "+ username);
+    }
+}
